d87d268bb8a879270ec2dfc4c36929b2baf65e22
GeoWill <roper.william@gmail.com>
2022-04-20 11:42:05 +0100

    Update scraper

--- scraper.py
+++ scraper.py
@@ -1,12 +1,12 @@
-from dc_base_scrapers.arcgis_scraper import ArcGisScraper
+from dc_base_scrapers.arcgis_scraper import UnsortedArcGisScraper
 
-
-stations_url = "https://gis.doncaster.gov.uk/arcgis/rest/services/ElectionSearch/FeatureServer/0/query?where=OBJECTID+LIKE+%27%25%27&objectIds=&time=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&gdbVersion=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=&resultOffset=&resultRecordCount=&f=pjson"
-districts_url = "https://gis.doncaster.gov.uk/arcgis/rest/services/ElectionSearch/FeatureServer/1/query?where=OBJECTID+LIKE+%27%25%27&objectIds=&time=&geometry=&geometryType=esriGeometryPolygon&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationParam=&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=4326&gdbVersion=&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=OBJECTID&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&multipatchOption=&resultOffset=&resultRecordCount=&f=pjson"
+root_url = "https://gis.doncaster.gov.uk/arcgis/rest/services/ElectionSearch/FeatureServer"
+stations_url = "{root_url}/0/query?where=OBJECTID+LIKE+'%25'&outFields=OBJECTID%2CPOLLINGPLA%2CUPRN%2CADDRESS%2CWARD%2CPOLLING_DI&geometryType=esriGeometryPoint&spatialRel=esriSpatialRelIntersects&units=esriSRUnit_Meter&returnGeometry=true&outSR=4326&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnZ=false&returnM=false&f=pjson".format(root_url=root_url)
+districts_url = "{root_url}/0/query?where=OBJECTID+LIKE+'%25'&outFields=OBJECTID%2CCODE&geometryType=esriGeometryPoint&spatialRel=esriSpatialRelIntersects&units=esriSRUnit_Meter&returnGeometry=true&outSR=4326&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnZ=false&returnM=false&f=pjson".format(root_url=root_url)
 council_id = 'DNC'
 
 
-stations_scraper = ArcGisScraper(stations_url, council_id, 'utf-8', 'stations')
+stations_scraper = UnsortedArcGisScraper(stations_url, council_id, 'utf-8', 'stations')
 stations_scraper.scrape()
-districts_scraper = ArcGisScraper(districts_url, council_id, 'utf-8', 'districts')
+districts_scraper = UnsortedArcGisScraper(districts_url, council_id, 'utf-8', 'districts')
 districts_scraper.scrape()
