7e2d751c413013ba8dc6a69287c86ae3ad5e74ee
GeoWill <roper.william@gmail.com>
2022-04-20 15:02:38 +0100

    Fix districts url

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ from dc_base_scrapers.arcgis_scraper import UnsortedArcGisScraper
 
 root_url = "https://gis.doncaster.gov.uk/arcgis/rest/services/ElectionSearch/FeatureServer"
 stations_url = "{root_url}/0/query?where=OBJECTID+LIKE+'%25'&outFields=OBJECTID%2CPOLLINGPLA%2CUPRN%2CADDRESS%2CWARD%2CPOLLING_DI&geometryType=esriGeometryPoint&spatialRel=esriSpatialRelIntersects&units=esriSRUnit_Meter&returnGeometry=true&outSR=4326&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnZ=false&returnM=false&f=pjson".format(root_url=root_url)
-districts_url = "{root_url}/0/query?where=OBJECTID+LIKE+'%25'&outFields=OBJECTID%2CCODE&geometryType=esriGeometryPoint&spatialRel=esriSpatialRelIntersects&units=esriSRUnit_Meter&returnGeometry=true&outSR=4326&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnZ=false&returnM=false&f=pjson".format(root_url=root_url)
+districts_url = "{root_url}/1/query?where=OBJECTID+LIKE+'%25'&outFields=OBJECTID%2CCODE&geometryType=esriGeometryPoint&spatialRel=esriSpatialRelIntersects&units=esriSRUnit_Meter&returnGeometry=true&outSR=4326&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnZ=false&returnM=false&f=pjson".format(root_url=root_url)
 council_id = 'DNC'
 
 
